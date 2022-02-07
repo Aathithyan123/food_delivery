a9abdc464fb217b2949a53dcf1afd2417b6338ec
Aathithyan123 <Techhunter113@gmail.com>
2022-02-08 00:00:41 +0530

    Initial commit

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight / 2.64;
+  static double pageViewContainer = screenHeight / 3.84;
+  static double pageViewTextContainer = screenHeight / 7.03;
+
+  //dynamic height padding and margin
+  static double height10 = screenHeight / 84.4;
+  static double height15 = (height10 / 2) * 3;
+  static double height20 = height10 * 2;
+  static double height30 = height10 * 3;
+  static double height45 = height30 + height15;
+
+  //dynamic width padding and margin
+  static double width10 = screenHeight / 84.4;
+  static double width15 = (height10 / 2) * 3;
+  static double width20 = height10 * 2;
+  static double width30 = height10 * 3;
+
+  static double font20 = screenHeight / 42.2;
+
+  //radius
+  static double radius20 = screenHeight / 42.2;
+  static double radius30 = screenHeight / 28.13;
+  static double radius15 = radius30 / 2;
+
+  //icon Size
+  static double iconsSize24 = screenHeight / 35.17;
+}
--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class BigText extends StatelessWidget {
-  final Color? color;
+  Color? color;
   final String text;
   double size;
   TextOverflow overflow;
   BigText(
       {Key? key,
-      this.color,
+      this.color = const Color(0xFF332d2b),
       required this.text,
-      required this.size,
+      this.size = 0,
       this.overflow = TextOverflow.ellipsis})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text);
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overflow,
+      style: TextStyle(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Roboto',
+        fontSize: size == 0 ? Dimensions.font20 : size,
+      ),
+    );
   }
 }
