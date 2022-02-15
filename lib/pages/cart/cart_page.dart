fd3f8747c2284f2a0d5f191179f95a9a9c6d0587
Aathithyan123 <Techhunter113@gmail.com>
2022-02-15 16:55:49 +0530

    Initial commit

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+import 'package:get/get.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -55,103 +57,111 @@ class CartPage extends StatelessWidget {
               child: MediaQuery.removePadding(
                 context: context,
                 removeTop: true,
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (_, index) {
-                      return Container(
-                        height: Dimensions.height20 * 5,
-                        width: double.maxFinite,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              width: Dimensions.height20 * 5,
-                              height: Dimensions.height20 * 5,
-                              margin:
-                                  EdgeInsets.only(bottom: Dimensions.height10),
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                        "assets/image/food0.png",
-                                      )),
-                                  borderRadius: BorderRadius.circular(
-                                      Dimensions.radius20),
-                                  color: Colors.white),
-                            ),
-                            SizedBox(
-                              width: Dimensions.width10,
-                            ),
-                            Expanded(
-                                child: Container(
-                              height: Dimensions.height20 * 5,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  BigText(
-                                    text: "Bitter Orange Juice",
-                                    color: Colors.black54,
-                                  ),
-                                  SmallText(text: "Spicy"),
-                                  Row(
+                child: GetBuilder<CartController>(
+                  builder: (cartController) {
+                    return ListView.builder(
+                        itemCount: cartController.getItems.length,
+                        itemBuilder: (_, index) {
+                          return Container(
+                            height: Dimensions.height20 * 5,
+                            width: double.maxFinite,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  width: Dimensions.height20 * 5,
+                                  height: Dimensions.height20 * 5,
+                                  margin: EdgeInsets.only(
+                                      bottom: Dimensions.height10),
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: AssetImage(
+                                            "assets/image/food0.png",
+                                          )),
+                                      borderRadius: BorderRadius.circular(
+                                          Dimensions.radius20),
+                                      color: Colors.white),
+                                ),
+                                SizedBox(
+                                  width: Dimensions.width10,
+                                ),
+                                Expanded(
+                                    child: Container(
+                                  height: Dimensions.height20 * 5,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
                                     children: [
                                       BigText(
-                                        text: "\$ 33.0",
-                                        color: Colors.redAccent,
+                                        text: cartController
+                                            .getItems[index].name!,
+                                        color: Colors.black54,
                                       ),
-                                      Container(
-                                        padding: EdgeInsets.symmetric(
-                                          vertical: Dimensions.height10,
-                                          horizontal: Dimensions.width10,
-                                        ),
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(
-                                              Dimensions.radius20),
-                                          color: Colors.white,
-                                        ),
-                                        child: Row(
-                                          children: [
-                                            GestureDetector(
-                                              onTap: () {
-                                                // popularProduct.setQuantity(false);
-                                              },
-                                              child: Icon(
-                                                Icons.remove,
-                                                color: AppColors.signColor,
-                                              ),
+                                      SmallText(text: "Spicy"),
+                                      Row(
+                                        children: [
+                                          BigText(
+                                            text:
+                                                "\$ ${cartController.getItems[index].price}",
+                                            color: Colors.redAccent,
+                                          ),
+                                          Container(
+                                            padding: EdgeInsets.symmetric(
+                                              vertical: Dimensions.height10,
+                                              horizontal: Dimensions.width10,
                                             ),
-                                            SizedBox(
-                                              width: Dimensions.width10 / 2,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                      Dimensions.radius20),
+                                              color: Colors.white,
                                             ),
-                                            BigText(text: ""
-                                                // popularProduct.inCartItems.toString()
+                                            child: Row(
+                                              children: [
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    // popularProduct.setQuantity(false);
+                                                  },
+                                                  child: Icon(
+                                                    Icons.remove,
+                                                    color: AppColors.signColor,
+                                                  ),
                                                 ),
-                                            SizedBox(
-                                              width: Dimensions.width10 / 2,
+                                                SizedBox(
+                                                  width: Dimensions.width10 / 2,
+                                                ),
+                                                BigText(text: ""
+                                                    // popularProduct.inCartItems.toString()
+                                                    ),
+                                                SizedBox(
+                                                  width: Dimensions.width10 / 2,
+                                                ),
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    // popularProduct.setQuantity(true);
+                                                  },
+                                                  child: Icon(
+                                                    Icons.add,
+                                                    color: AppColors.signColor,
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                            GestureDetector(
-                                              onTap: () {
-                                                // popularProduct.setQuantity(true);
-                                              },
-                                              child: Icon(
-                                                Icons.add,
-                                                color: AppColors.signColor,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
+                                          ),
+                                        ],
+                                      )
                                     ],
-                                  )
-                                ],
-                              ),
-                            ))
-                          ],
-                        ),
-                      );
-                    }),
+                                  ),
+                                ))
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                ),
               ),
             ),
           )
